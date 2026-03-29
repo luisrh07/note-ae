77c98f37dc0d5392f5d2873f96645578b052884f
luisrh07 <143743133+luisrh07@users.noreply.github.com>
2026-03-29 16:44:22 -0600

    Refactor music controls and add song navigation

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
 let music = document.getElementById("music");
-let btn = document.getElementById("musicBtn");
+let playBtn = document.getElementById("playBtn");
+
+let songs = ["music.mp3"];
+let index = 0;
 let playing = false;
+
+music.src = songs[index];
 
 function toggleMusic() {
     if (!playing) {
         music.play();
-        btn.innerText = "Pausar música ⏸️";
+        playBtn.innerText = "⏸";
         playing = true;
     } else {
         music.pause();
-        btn.innerText = "Tocar música 🎵";
+        playBtn.innerText = "▶️";
         playing = false;
     }
 }
 
-/* Corazones flotando */
-setInterval(() => {
-    let heart = document.createElement("div");
-    heart.innerHTML = "💖";
-    heart.classList.add("heart");
+function nextSong() {
+    index = (index + 1) % songs.length;
+    music.src = songs[index];
+    music.play();
+}
 
-    heart.style.left = Math.random() * 100 + "vw";
-    heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+function prevSong() {
+    index = (index - 1 + songs.length) % songs.length;
+    music.src = songs[index];
+    music.play();
+}
 
-    document.body.appendChild(heart);
+/* SOBRE */
+function openEnvelope() {
+    let env = document.querySelector(".envelope");
+    let paper = document.getElementById("paper");
+
+    env.classList.add("open");
 
     setTimeout(() => {
-        heart.remove();
-    }, 4000);
-}, 500);
+        env.style.display = "none";
+        paper.classList.remove("hidden");
+        paper.classList.add("show");
+    }, 700);
+}
+
+/* MODO */
+function toggleMode() {
+    document.body.classList.toggle("light");
+}
+
+/* ESTRELLAS */
+setInterval(() => {
+    let star = document.createElement("div");
+    star.classList.add("star");
+    star.style.left = Math.random() * 100 + "vw";
+
+    document.body.appendChild(star);
+
+    setTimeout(() => star.remove(), 6000);
+}, 200);
+
+/* GIRASOLES */
+setInterval(() => {
+    let sun = document.createElement("div");
+    sun.classList.add("sunflower");
+    sun.innerHTML = "🌻";
+    sun.style.left = Math.random() * 100 + "vw";
+
+    document.body.appendChild(sun);
+
+    setTimeout(() => sun.remove(), 8000);
+}, 2000);
